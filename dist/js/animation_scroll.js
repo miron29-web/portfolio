fe9f8297607d2485aa6309f04e065f941d7f13cc
Sergey <miron29.web@gmail.com>
2026-08-17 10:31:43 +0300

    'deploy'

---
+++ dist/js/animation_scroll.js
@@ -0,0 +1,22 @@
+const animItems = document.querySelectorAll("._anim-item");
+
+if (animItems.length) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("_active");
+        } else if (!entry.target.classList.contains("_anim-no-hide")) {
+          entry.target.classList.remove("_active");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    },
+  );
+
+  animItems.forEach((item) => {
+    observer.observe(item);
+  });
+}
